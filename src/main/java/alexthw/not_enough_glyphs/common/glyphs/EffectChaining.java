5983e7a9a6a60281325d63c4bdf1a63325d5ddfb
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2024-09-04 00:13:53 +0200

    Update flatten art, adjust displayed modid by jei

--- src/main/java/alexthw/not_enough_glyphs/common/glyphs/EffectChaining.java
+++ src/main/java/alexthw/not_enough_glyphs/common/glyphs/EffectChaining.java
@@ -1,6 +1,11 @@
 package alexthw.not_enough_glyphs.common.glyphs;
 
 import com.hollingsworth.arsnouveau.api.spell.*;
+import alexthw.not_enough_glyphs.common.network.PacketRayEffect;
+import alexthw.not_enough_glyphs.init.NotEnoughGlyphs;
+import com.hollingsworth.arsnouveau.api.spell.*;
+import com.hollingsworth.arsnouveau.common.items.Glyph;
+import com.hollingsworth.arsnouveau.common.network.Networking;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentAOE;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentPierce;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentSensitive;
@@ -12,6 +17,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
@@ -19,6 +25,9 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ForgeConfigSpec;
+
+import org.jetbrains.annotations.NotNull;
+
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -268,4 +277,16 @@ public class EffectChaining extends AbstractEffect {
         return selectedEdges;
     }
 
+    @Override
+    public Glyph getGlyph() {
+        if (glyphItem == null) {
+            glyphItem = new Glyph(this) {
+                @Override
+                public @NotNull String getCreatorModId(@NotNull ItemStack itemStack) {
+                    return NotEnoughGlyphs.MODID;
+                }
+            };
+        }
+        return this.glyphItem;
+    }
 }
