7313b1efba7c1e6a833efd49d4ddb4697397c8a5
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2025-06-01 23:43:36 +0200

    fix missing lang for binder in docs

--- src/main/java/alexthw/not_enough_glyphs/datagen/NEGLangProvider.java
+++ src/main/java/alexthw/not_enough_glyphs/datagen/NEGLangProvider.java
@@ -58,7 +58,7 @@ public class NEGLangProvider extends LanguageProvider {
         add("not_enough_glyphs.perk.mana_discount.desc", "Reduces the mana cost of the spell by %s.");
         add("ars_nouveau.page.focus_threads", "Book Thread: Focus");
         add("ars_nouveau.page.focus_threads.desc", "Having these threads on the spellbinder will allow to cast spells as if the focus was equipped.");
-        add("ars_nouveau.page.spell_binder", "An alternative to the traditional spellcasting, the spellbinder is a tool that allows to cast spells from the spell parchemnts and caster tomes put inside it. The left side will hold the 10 spells for the radial menu (V), while the right side allow to keep additional 15 spells to switch when needed.");
+        add("not_enough_glyphs.page.spell_binder", "An alternative to the traditional spellcasting, the spellbinder is a tool that allows to cast spells from the spell parchemnts and caster tomes put inside it. The left side will hold the 10 spells for the radial menu (V), while the right side allow to keep additional 15 spells to switch when needed.");
         add("ars_nouveau.page.book_threads", "Book Threads");
         add("not_enough_glyphs.page.book_threads.desc", "The book threads are at the core of the spellbinder. They allow to give your binder special properties and effects, similar to how Armor Threads work. You can use an Alteration Table to slot up to two threads into the Spell Binder.");
 
